a95cf5b02872b13f0c3e84e671c8ca4cdeb66038
Kia Fathi <kiafathi@me.com>
2014-05-15 12:38:32 -0700

    Began the implementation of stringifyJSON function

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,6 +2,21 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
+
+
 var stringifyJSON = function(obj) {
   // your code goes here
+  var res;
+  if(typeof obj === 'number'){
+  	res = obj.toString();
+  }else if(typeof obj === 'object'){
+  	res = "" + obj + "";
+  }else if(typeof obj === 'undefined'){
+  	res = 'undefined';
+  }else if(typeof obj === 'boolean'){
+  	res = obj.toString();
+  }else if(typeof obj === 'string'){
+  	res = '"' + obj + '"';
+  }
+  return res;
 };
