61dcf9056caa07f94fa2517cee792cf2402969f0
Kia Fathi <kiafathi@me.com>
2014-05-15 14:12:57 -0700

    Further improvements of my stringifyJSON function

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -7,16 +7,31 @@
 var stringifyJSON = function(obj) {
   // your code goes here
   var res;
-  if(typeof obj === 'number'){
+  if(obj === null){
+  	res = "null";
+  }else if(typeof obj === 'number'){
   	res = obj.toString();
-  }else if(typeof obj === 'object'){
-  	res = "" + obj + "";
-  }else if(typeof obj === 'undefined'){
-  	res = 'undefined';
   }else if(typeof obj === 'boolean'){
   	res = obj.toString();
+  }else if(typeof obj === 'undefined'){
+  	res = 'undefined';
   }else if(typeof obj === 'string'){
   	res = '"' + obj + '"';
+  }else if(typeof obj === 'object'){
+  	if(obj.length >= 0){
+  		var str = ""
+  		for(var i = 0; i < obj.length; i++){
+  			var cur = obj[i];
+  			if(i === 0){
+  				str += stringifyJSON(cur);
+  			}else{
+  				str += "," + stringifyJSON(cur);
+  			}	
+  		}
+  		res = "[" + str + "]";
+  	}else{
+  		res = "" + obj + "";
+  	}
   }
   return res;
 };
