767b0e2780b197a5351135cb2bc3591dc2e81889
Kia Fathi <kiafathi@me.com>
2014-05-16 12:34:13 -0700

    getElementsByClassName now fulfills all the spec requirements

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -8,28 +8,21 @@ var getElementsByClassName = function(className){
   var res = [];
   var scanner = function(arg){
   	if(arg.classList != undefined && arg.classList.length > 0){
-  		if(arg.classList.contains(className)){
+      if(arg.classList.contains(className)){
   			res.push(arg);
   		}
   	}
-    console.log('res after first if = ');
-    console.log(res);
   	if(arg.hasChildNodes()){
   		var children = arg.childNodes;
       alert('gets here')
 		for(var node in children){
-  			if(typeof node != 'undefined'){
-  				scanner(node);
+  			if(typeof children[node] == 'object'){
+  				scanner(children[node]);
   			}
   		}
   	}
   }
   scanner(document.body);
-
-  console.log("res at completion is: ");
-  console.log(res);
-  console.log("res should equal: ");
-  console.log(document.getElementsByClassName('targetClassName'));
   return res;
 };
  
