c9f61c2e84b1f8c60f7f98e351805422f0f01fde
Kia Fathi <kiafathi@me.com>
2014-05-15 18:50:51 -0700

    Further refinments of getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,35 +6,27 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
   var res = [];
-  var count1 = 0
-  var count2 = 0;
   var scanner = function(arg){
   	var cur = arg;
-  	if(cur.classList.length > 0){
+  	if(cur.classList != undefined && cur.classList.length > 0){
   		for(var i = 0; i < arg.classList.length; i++){
-  			if(arg.classList[i] == className){
+  			if(arg.classList[i] === className){
   				res.push(arg);
-  				console.log('res' + res);
-  				count1 ++;
-  				alert('gets to if1 times: ' + count1);
   			}
   		}
   	}
   	if(arg.hasChildNodes()){
   		var children = arg.childNodes;
-  		console.log('These are the child nodes' + arg.childNodes);
 		for(var i; i < children.length; i++){
-			console.log('current scanning: ' + children[i]);
   			if(typeof children[i] != 'undefined'){
-  				count2 ++;
-  				alert('Get to if2 times: ' + count2);
   				scanner(children[i]);
   			}
-  			
   		}
   	}
   }
   scanner(document.body);
 
+  console.log("res at completion is: " + res );
+  console.log("res should equal: " + document.getElementsByClassName('targetClassName'));
   return res;
 };
